e528629489f637cb772327821b74092ec9221798
vvbushiwo <1411454222@qq.com>
2019-09-17 15:35:20 +0800

    190916

--- Stack/如何实现队列.py
+++ Stack/如何实现队列.py
@@ -6,7 +6,7 @@
 2、链表实现
 """
 
-
+"""
 # 数组实现
 class MyQueue:
     def __init__(self):
@@ -46,6 +46,70 @@ class MyQueue:
         self.arr.append(item)
         self.rear += 1
 
+"""
+
+
+class LNode:
+    def _new_(self, x):
+        self.data = x
+        self.next = None
+
+
+class MyQueue:
+    # 分配头节点
+    def __init__(self):
+        self.pHead = None
+        self.pEnd = None
+
+    # 判断队列是否为空，如果为空返回true，否则返回false
+    def empty(self):
+        if self.pHead is None:
+            return True
+        else:
+            return False
+
+    # 获取栈中元素的个数
+    def size(self):
+        size = 0
+        p = self.pHead
+        while p is not None:
+            p = p.next
+            size += 1
+        return size
+
+    # 入队列：把元素e加到队列尾
+    def add_queue(self, e):
+        p = LNode()
+        p.data = e
+        p.next = None
+        if self.pHead is None:
+            self.pHead = self.pEnd = p
+        else:
+            self.pEnd.next = p
+            self.pEnd = p
+
+    # 出队列，删除队列的首元素
+    def de_queue(self):
+        if self.pHead is None:
+            print("出队列失败，队列已经为空")
+        self.pHead = self.pHead.next
+        if self.pHead is None:
+            self.pEnd is None
+
+    # 取得队列首元素
+    def get_front(self):
+        if self.pHead is None:
+            print("获取队列首元素失败，队列已经为空")
+            return None
+        return self.pHead.data
+
+    # 取得队列尾元素
+    def get_rear(self):
+        if self.pEnd is None:
+            print("获取队列尾元素失败，队列为空")
+            return None
+        return self.pEnd.data
+
 
 if __name__ == "__main__":
     queue = MyQueue()
@@ -54,3 +118,4 @@ if __name__ == "__main__":
     print("队列头元素为：" + str(queue.get_front()))
     print("队列尾元素：" + str(queue.get_rear()))
     print("队列大小" + str(queue.size()))
+

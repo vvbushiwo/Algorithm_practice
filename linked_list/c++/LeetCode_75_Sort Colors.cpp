d3e43181c1400c7aeca1899740cf725d0f4dba74
vvbushiwo <1411454222@qq.com>
2023-04-25 13:14:54 +0300

    230425 Array problems of LeetCode.

---
+++ linked_list/c++/LeetCode_75_Sort Colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        int index = 0;
+        while(index <= right){
+            if(nums[index] == 0){
+                swap(nums[index], nums[left]);
+                left++;
+                index++;
+            }
+            else if(nums[index] == 2){
+                swap(nums[index], nums[right]);
+                right--;
+            }
+            else{
+                index++;
+            }
+        }
+        
+
+        
+    }
+};
